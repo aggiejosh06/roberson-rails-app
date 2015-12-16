5e571c1e700c51980b477b3f8af86e9adc28072f
Joshua Roberson <aggiejosh06@gmail.com>
2015-12-16 11:18:38 -0600

    Added validation requirements to form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,29 @@
 class ContactsController < ApplicationController
-# This action will look in the views folder for an html file called new.html.erb.
+	# This action will look in the views folder for an html file called new.html.erb.
 	def new
-# This creates a new contact object and saves it on the web server.	This is a specific user object.	
+		# This creates a new contact object and saves it on the web server.	This is a specific user object.	
 		@contact = Contact.new
 	end
 	
-# This action will look in the views folder for an html file called create.html.erb.
+	# This action will look in the views folder for an html file called create.html.erb.
 	def create
+		@contact = Contact.new(contact_params)
+
+		# This ruby method saves the contact information
+		# @contact.save results in true
+		if @contact.save
+			flash[:success] = "Message sent."
+			redirect_to new_contact_path
+		# @contact.save results in false
+		else
+			flash[:danger] = "Error occured, message has not been sent."
+			redirect_to new_contact_path
+		end
 	end
+
+	private
+		def contact_params
+			#This white lists the parameters. I have no idea what that means.
+			params.require(:contact).permit(:name, :email, :comments)
+		end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
-
+	#These actions? make sure that someone actually puts in a response for both name and e-mail.
+	validates :name, presence: true
+	validates :email, presence: true
 end
