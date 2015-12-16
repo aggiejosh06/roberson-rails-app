c7f90acf6ee29a0267264ec2d2eda65256a8799d
Joshua Roberson <aggiejosh06@gmail.com>
2015-12-16 10:20:45 -0600

    Added contact page to nav bar

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,11 @@
 class ContactsController < ApplicationController
+# This action will look in the views folder for an html file called new.html.erb.
 	def new
+# This creates a new contact object and saves it on the web server.	This is a specific user object.	
 		@contact = Contact.new
 	end
-
+	
+# This action will look in the views folder for an html file called create.html.erb.
 	def create
 	end
 end
